7149f8efae4626ba331df72368388b647a646b64
Amelia_Tan <ameliatanyr@gmail.com>
2020-12-23 01:15:44 +0800

    Adding a route for comments and generating contoller Comments

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
+  resources :articles do
+    # added the comments thing
+  	# This creates comments as a nested resource within articles. 
+  	# This is another part of capturing the hierarchical relationship 
+  	# that exists between articles and comments.
+    resources :comments
+  end
 end
