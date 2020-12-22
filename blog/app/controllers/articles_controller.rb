e3305a98a1405eb1463c7c1c2eb13ed7f40a95e7
Amelia_Tan <ameliatanyr@gmail.com>
2020-12-23 00:57:26 +0800

    Allowing edits via partials
    - add the edit and update functions
    - use the partial in place of the new form (does not depend on
      specific instance variables set by a controller action)
    - create a similar edit.html.erb

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -12,13 +12,26 @@ class ArticlesController < ApplicationController
   end
 
   def create
-  	# now strong typing is used to prevent malicious input
-    @article = Article.new(article_params) 
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
     else
       render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
     end
   end
 
